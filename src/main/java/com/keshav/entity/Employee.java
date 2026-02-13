cd7e6cad8ae1aba95223a64bef99cb7cbf944c09
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-02-13 15:27:49 +0530

    update

--- src/main/java/com/keshav/entity/Employee.java
+++ src/main/java/com/keshav/entity/Employee.java
@@ -1,7 +1,17 @@
 package com.keshav.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "myEmp")
 public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(name = "empName")
 	private String name;
 	private String gender;
 	private int salary;
--- src/main/java/com/keshav/main/Main.java
+++ src/main/java/com/keshav/main/Main.java
@@ -3,12 +3,8 @@ package com.keshav.main;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
+import com.keshav.configuration.HibernateConfig;
 import com.keshav.entity.Employee;
 
 public class Main {
@@ -16,14 +12,10 @@ public class Main {
 	public static void main(String[] args) {
 		Employee e = new  Employee( "Kunal", "male", 22200);
 		
-		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernatee.cfg.xml").build();
-		Metadata metaData = new MetadataSources(ssr).getMetadataBuilder().build();
-		SessionFactory sf = metaData.buildSessionFactory();
+		SessionFactory sf = HibernateConfig.getSessionFactory();
+		
 		Session session = sf.openSession();
 		Transaction tx = session.beginTransaction();
-		
-		
-		
 		session.persist(e);
 		tx.commit();
 	}
